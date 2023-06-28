8e388580c41f5a28d72bf46a47de0284129495ca
Peterkenya <abilapeter@gmail.com>
2023-06-28 06:26:35 -0700

    Hello,world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h.
+/**
+ *main -prints the phase "programming is like building a multilingual puzzle
+ * Return:0 if exited property, non-zero otherwise
+ */
+int main(void)
+{puts("\"Programming is like building a multilingual puzzle");
+}`
